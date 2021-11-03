bfeba2c483d33494a8f184af3ef74ffa2a1ef741
Giang <kaylinpham109@gmail.com>
2021-11-03 15:33:02 +0700

    defining routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAmHsh1SROd9c2uCOJtF3TjGob8eqQHbeM",
+    authDomain: "oshop-74697.firebaseapp.com",
+    projectId: "oshop-74697",
+    storageBucket: "oshop-74697.appspot.com",
+    messagingSenderId: "798025081188",
+    appId: "1:798025081188:web:6c4712498efcf36425da71",
+    measurementId: "G-VC7H0R310D"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAmHsh1SROd9c2uCOJtF3TjGob8eqQHbeM",
+    authDomain: "oshop-74697.firebaseapp.com",
+    projectId: "oshop-74697",
+    storageBucket: "oshop-74697.appspot.com",
+    messagingSenderId: "798025081188",
+    appId: "1:798025081188:web:6c4712498efcf36425da71",
+    measurementId: "G-VC7H0R310D"
+  }
 };
 
 /*
