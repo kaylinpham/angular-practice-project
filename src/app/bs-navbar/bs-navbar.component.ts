40c1c02667abcf072f4372cd84e1d316f8b4e0ab
Giang <kaylinpham109@gmail.com>
2021-11-04 10:29:47 +0700

    display user and protect some routes

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Auth, authState, signOut } from '@angular/fire/auth';
+import { User } from "firebase/auth"
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
+  user: User | null = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private auth: Auth) {
+    authState(auth).subscribe(user => this.user = user)
   }
 
+  logout() { signOut(this.auth) }
 }
