c6041158dc59f7729f59dd17755ac9ba5e85a518
Giang <kaylinpham109@gmail.com>
2021-11-05 22:56:03 +0700

    using async pipe

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Auth, authState, signOut } from '@angular/fire/auth';
 import { User } from "firebase/auth"
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bs-navbar',
@@ -8,10 +9,11 @@ import { User } from "firebase/auth"
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-  user: User | null = null;
+  user$: Observable<User | null>;
 
   constructor(private auth: Auth) {
-    authState(auth).subscribe(user => this.user = user)
+    this.user$ = authState(auth)
+    //using Observable to unsubscribe automatically
   }
 
   logout() { signOut(this.auth) }
